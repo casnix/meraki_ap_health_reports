463891c7a0272d207758a2333db288912cf1c242
Matt Rienzo <casnix@users.noreply.github.com>
2026-06-06 19:20:40 -0500

    created run script.

--- run.sh
+++ run.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 # Assume environment variable for token is MERAKI_API_TOKEN.
+#MERAKI_API_TOKEN=YOUR TOKEN HERE
+
+# Assume environment variable for the outbound email server is in MERAKI_HEALTH_REPORT_MAIL_SERVER
+#MERAKI_HEALTH_REPORT_MAIL_SERVER=YOU.EMAIL.SERVER.NET
+
+# Assume environment variable for the from: email address is in MERAKI_HEALTH_REPORT_FROM
+#MERAKI_HEALTH_REPORT_FROM=meraki_ap_health_reports@localhost
+
+# Assume environment variable for the destination email address is in MERAKI_HEALTH_REPORT_TO
+#MERAKI_HEALTH_REPORT_TO=admin@localhost
 
 # Test if we have a report stored.
 if [ -f "./json/recent.json "]; then
@@ -10,8 +20,25 @@ if [ -f "./report/recent.pdf"]; then
     rm ./report/recent.pdf
 fi
 
-# Run the crawler
-python3 ./src/meraki_ap_crawler.py --api-key $MERAKI_API_TOKEN --all-orgs --output ./json/recent.json
-# Make the report
-python3 ./src/meraki_ap_report.py --input ./json/recent.json --output ./report/recent.pdf
+# Enter venv
+source ./venv/bin/activate
 
+# Do work
+python3 ./src/meraki_ap_crawler.py \
+    --api-key $MERAKI_API_TOKEN \
+    --all-orgs \
+    --output ./json/recent.json
+python3 ./src/meraki_ap_report.py \
+    --input ./json/recent.json \
+    --output ./report/recent.pdf
+python ./src/send_email.py \
+  --server $MERAKI_HEALTH_REPORT_MAIL_SERVER \
+  --port 25 \
+  --from $MERAKI_HEALTH_REPORT_FROM \
+  --to $MERAKI_HEALTH_REPORT_TO \
+  --title "Meraki AP Report" \
+  --message "See attached." \
+  --attachment ./report/recent.pdf
+
+# Exit venv
+deactivate
